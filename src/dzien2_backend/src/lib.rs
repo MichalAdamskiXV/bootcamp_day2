104277fcd3d2aaf0ccc8fd74fe3f3118b9c60350
MichalAdamskiXV <madamskixv@gmail.com>
2024-06-25 18:54:22 +0200

    added way to add wpisy

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,9 +1,31 @@
-#[ic_cdk::query]
+use std::cell::RefCell;
 
-//typy danych
-//u8 - liczba od 0 do 255
-//i8 - liczba od -127 do 128 
+//sekcja danych
+thread_local! {
+    //static to pole w pamięci, które będzie żyło cały czas
+    // RefCell - można korzystać z danych ze zmiennej statycznej
+    // Vec - wektor stringów
+    //WPISY - lista stringów 
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
+#[ic_cdk::query]
+// ic_cdk::query - metoda odczytu danych dot. funkjcjii poniżej 
+// typy danych
+// u8 - liczba od 0 do 255
+// i8 - liczba od -127 do 128 
 
 fn greet(name: String, last_name: i8) -> String {
     format!("Hello, {} {}!", name, last_name)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String) {
+    //mamy zmienną wpis
+    //wpychamy ją do wszystkich wpisów (do tego wektora)
+    //bierzemy WPISY - with przyjmuje funkcje (wpisy to zmienna wypakowana)
+    //wpisy.borrow_mut() - porzyczamy zmienną i zwracamy .push(wpis)
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow_mut().push(wpis);
+    });
 }
