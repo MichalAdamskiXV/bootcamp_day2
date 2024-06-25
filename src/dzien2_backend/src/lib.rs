2afd644cd0c852ddedd54d44af3f9a593abc504d
MichalAdamskiXV <madamskixv@gmail.com>
2024-06-25 17:51:36 +0200

    first commit

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -1,4 +1,9 @@
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+
+//typy danych
+//u8 - liczba od 0 do 255
+//i8 - liczba od -127 do 128 
+
+fn greet(name: String, last_name: i8) -> String {
+    format!("Hello, {} {}!", name, last_name)
 }
