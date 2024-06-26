c605e074e50536d01a61480a608e71905f457690
MichalAdamskiXV <madamskixv@gmail.com>
2024-06-26 19:36:46 +0200

    blog

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy Na Bloga</h2>
-        <button @click="pobierzWpisy">Refresh</button>
-        <div v-for="wpis in wpisy">
-            <p>{{wpis}}</p>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded-sm text-white p-4">Refresh</button>
         </div>
-        {{ wpisy }}
-        <input type="text" v-model="nowyBlog">
-        <button @click="dodajWpis">Dodaj</button>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>ID : {{index}}</p>
+                <p>{{wpis}}</p>
+            </div>
+        </div>
+        <div class="grid items-center justify-center gap-4 flex-col">
+            <input type="text" v-model="nowyBlog" class="border-2 border-blue-600 p-4">
+            <button class="bg-blue-600 rounded-sm text-white p-4" @click="dodajWpis">Dodaj</button>
+        </div>
     </div>
 </template>
 
